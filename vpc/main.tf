8868e407417dd1938d51fc0c8dbc19cde9bb7f21
picou75 <jeanne.gardebois@gmail.com>
2020-12-14 16:21:45 +0100

    update security group to accept http + https

--- vpc/main.tf
+++ vpc/main.tf
@@ -99,8 +99,15 @@ resource "aws_security_group" "vpc_sg" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
